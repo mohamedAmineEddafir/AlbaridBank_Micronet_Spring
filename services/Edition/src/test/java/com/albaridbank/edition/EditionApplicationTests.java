ebae4de6a43b10129fb90062603cf89eef12bef3
eddafir.mohamed.amine01 <eddafir.mohamed.amine01@gmail.com>
2025-03-27 22:20:25 +0000

    feat(microservice): add Edition microservice for reporting and financial data analysis
    Added the Edition microservice, which plays a crucial role in the AlbaridBank architecture. This service focuses on reporting, financial data analysis, and document generation for various stakeholders. It enhances data insights and improves the bank’s analytical capabilities.

---
+++ services/Edition/src/test/java/com/albaridbank/edition/EditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albaridbank.edition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
