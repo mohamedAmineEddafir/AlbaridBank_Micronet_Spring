9ab39add9ea5006b3b79877f994f5c6bf7f79ae7
eddafir.mohamed.amine01 <eddafir.mohamed.amine01@gmail.com>
2025-04-05 16:49:40 +0000

    Adds (Repository): ParameterRapportRepoitory interface
    Creates ParameterRapportRepository interface extending JpaRepository to handle database operations for ParametreRapport entities.

---
+++ services/Edition/src/main/java/com/albaridbank/edition/repositorys/ParameterRapportRepository.java
@@ -0,0 +1,7 @@
+package com.albaridbank.edition.repositorys;
+
+import com.albaridbank.edition.model.ParametreRapport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParameterRapportRepository extends JpaRepository<ParametreRapport, Long> {
+}
