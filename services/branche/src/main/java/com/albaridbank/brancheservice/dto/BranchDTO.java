2d39f15cc268f215e637ef3df7c95099e3c406f4
eddafir.mohamed.amine01 <eddafir.mohamed.amine01@gmail.com>
2025-04-08 00:26:40 +0100

    Adds (BranchDTO)
    Introduces the BranchDTO class to represent branch data.
    
    This DTO includes attributes such as branch codes, names, types,
    region, zone, group, locality, categories, status, deployment
    information, responsible person, IP address, connection type, address,
    postal code, phone numbers, and email.
    
    It uses Lombok annotations for boilerplate code generation
    (e.g., getters, setters, constructors, builder).

---
+++ services/branche/src/main/java/com/albaridbank/brancheservice/dto/BranchDTO.java
@@ -0,0 +1,41 @@
+package com.albaridbank.brancheservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BranchDTO {
+
+    private String codeAgence;            // codburpo
+    private String nomAgence;             // libelleBurpo
+    private String typeAgence;            // typbur
+    private String region;                // libreg
+    private Integer codeRegion;           // codreg
+    private String zone;                  // libelleZone
+    private String groupe;                // libelleGroupe
+    private String localite;              // libelleLocalite
+    private String categorie;             // categorie
+    private String catAgence;             // catagence
+
+    private String statut;                // statut (O/N)
+    private String deploye;               // deploye (O/N)
+    private LocalDate dateDeploiement;    // datedeploiement
+    private String aDeployer;             // adeployer (O/N)
+
+    private String responsable;           // responsable
+    private String ip;                    // ip
+    private String typeConnexion;         // typeconnexion
+
+    private String adresse;               // adresse
+    private String codePostal;            // codepostale
+    private String telephoneFixe;         // telephone_fixe
+    private String telephoneMobile;       // telephone_mobile
+    private String email;                 // email
+}
