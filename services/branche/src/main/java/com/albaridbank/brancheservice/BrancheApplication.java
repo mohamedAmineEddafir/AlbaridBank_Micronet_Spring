0779de03d31059bd3c5b7a528c3eb93b97618d42
eddafir.mohamed.amine01 <eddafir.mohamed.amine01@gmail.com>
2025-04-07 14:00:39 +0100

    Adds (branche-service) to the project
    This commit introduces a new microservice, "branche-service," to manage branch-related data and functionalities.
    
    It includes the initial project setup, Maven configurations, and basic application structure.

---
+++ services/branche/src/main/java/com/albaridbank/brancheservice/BrancheApplication.java
@@ -0,0 +1,13 @@
+package com.albaridbank.brancheservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrancheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrancheApplication.class, args);
+	}
+
+}
---
+++ services/branche/src/test/java/com/albaridbank/brancheservice/BrancheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albaridbank.brancheservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrancheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
