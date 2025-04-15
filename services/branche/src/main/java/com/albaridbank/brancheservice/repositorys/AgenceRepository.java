816456be13a8a961e532144403d32e4dc9269d2d
eddafir.mohamed.amine01 <eddafir.mohamed.amine01@gmail.com>
2025-04-15 16:51:55 +0100

    Improves agency search functionality
    Refines the agency search functionality by adding case-insensitive search capabilities for agency name and region.
    
    Replaces direct repository methods with custom queries using LIKE and LOWER functions for more flexible and user-friendly searches.
    
    findByLibelleRegion now uses a custom query to perform a case-insensitive search.
    
    Introduces a new method, findByNomAgenceContainingIgnoreCase, for case-insensitive search by agency name.
    
    Removes findByLibelleZone method.

--- services/branche/src/main/java/com/albaridbank/brancheservice/repositorys/AgenceRepository.java
+++ services/branche/src/main/java/com/albaridbank/brancheservice/repositorys/AgenceRepository.java
@@ -3,6 +3,8 @@ package com.albaridbank.brancheservice.repositorys;
 import com.albaridbank.brancheservice.model.SgcRefAgence;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -30,14 +32,6 @@ public interface AgenceRepository extends JpaRepository<SgcRefAgence, String>, J
     List<SgcRefAgence> findByLibelleBurpo(String libelleBurpo);
 
     /**
-     * Finds an {@link SgcRefAgence} entity by its libelleZone '{@summary Zone géographique}' (zone name).
-     *
-     * @param libelleZone The zone name to search for.
-     * @return The matching {@link SgcRefAgence} entity, or null if not found.
-     */
-    List<SgcRefAgence> findByLibelleZone(String libelleZone);
-
-    /**
      * Finds an {@link SgcRefAgence} entity by its libelleGroupe '{@summary Groupe auquel elle appartient}' (group name).
      *
      * @param libelleGroupe The group name to search for.
@@ -56,27 +50,31 @@ public interface AgenceRepository extends JpaRepository<SgcRefAgence, String>, J
     /**
      * Finds an {@link SgcRefAgence} entity by its libelleRegion '{@summary Region}' (region name).
      *
-     * @param libelleRegion The region name to search for.
+     * @param region The region name to search for.
      * @return The matching {@link SgcRefAgence} entity, or null if not found.
      */
-    List<SgcRefAgence> findByLibelleRegion(String libelleRegion);
+    //List<SgcRefAgence> findByLibelleRegion(String libelleRegion);
+    @Query("SELECT b FROM SgcRefAgence b WHERE LOWER(TRIM(b.libelleRegion)) LIKE LOWER(CONCAT('%', TRIM(:region), '%'))")
+    List<SgcRefAgence> findByLibelleRegion(@Param("region") String region);
 
     /**
-     * Finds agencies by their status.
+     * Finds an {@link SgcRefAgence} entity by its libelleBurpo '{@summary  Nom ou intitule de l'agence}' (agency name).
+     * This method performs a case-insensitive search.
      *
-     * @param statut The status to search for (e.g., 'O' for Ouvrir)
-     * @return List of matching agencies
+     * @param nomAgence The agency name to search for.
+     * @return A list of matching {@link SgcRefAgence} entities.
      */
-    List<SgcRefAgence> findByStatut(String statut);
+    @Query("SELECT b FROM SgcRefAgence b WHERE LOWER(TRIM(b.libelleBurpo)) LIKE LOWER(CONCAT('%', TRIM(:nomAgence), '%'))")
+    List<SgcRefAgence> findByNomAgenceContainingIgnoreCase(@Param("nomAgence") String nomAgence);
 
     /**
-     * Finds agencies by their status and group.
+     * Finds agencies by their status and region.
      *
      * @param statut The status to search for (e.g., 'O' for Ouvrir)
-     * @param groupe The group to search for
+     * @param libelleRegion The region to search for
      * @return List of matching agencies
      */
-    List<SgcRefAgence> findByStatutAndGroupe(String statut, String groupe);
+    List<SgcRefAgence> findByStatutAndLibelleRegion(String statut, String libelleRegion);
 
     /**
      * Counts the number of agencies by status.
