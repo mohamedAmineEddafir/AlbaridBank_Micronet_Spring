0d36822bdeedbf55e14bad8a2ffbca490dd42a4d
eddafir.mohamed.amine01 <eddafir.mohamed.amine01@gmail.com>
2025-04-15 16:52:41 +0100

    Refactors branch retrieval by status
    Updates the endpoint for retrieving branches by status to include pagination and sorting.
    
    Removes unused endpoints for retrieving branches by zone, group, locality, and status/group combinations.
    
    Adds a new endpoint to retrieve simple branch information by agency code.

--- services/branche/src/main/java/com/albaridbank/brancheservice/controller/BrancheController.java
+++ services/branche/src/main/java/com/albaridbank/brancheservice/controller/BrancheController.java
@@ -18,7 +18,7 @@ import java.util.List;
  * REST controller for managing branches.
  * Provides endpoints for retrieving branch information with various filters and pagination.
  *
- * @Author Mohamed Amine Eddafir
+ * @author Mohamed Amine Eddafir
  */
 @RestController
 @RequestMapping("/api/v1/branches")
@@ -66,10 +66,15 @@ public class BrancheController {
      * @return A list of branches with the specified status.
      */
     @GetMapping("/statut/{statut}")
-    public ResponseEntity<List<BranchDTO>> getBranchesBystatut(
-            @PathVariable String statut) {
-        log.info("REST request to get Branches with statut: {}", statut);
-        return ResponseEntity.ok(agenceService.getBranchesBystatut(statut));
+    public ResponseEntity<Page<BranchDTO>> getBranchesBystatut(
+            @PathVariable String statut,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "codburpo") String sort)
+    {
+        log.info("Getting all branches with statut: page={}, size={}, sort={}", page, size, sort);
+        Pageable pageable = PageRequest.of(page, size, Sort.by(sort));
+        return ResponseEntity.ok(agenceService.getBranchesBystatut(pageable ,statut));
     }
 
     /**
@@ -83,45 +88,6 @@ public class BrancheController {
             @RequestParam String nomAgence) {
         log.info("REST request to search Branches by name: {}", nomAgence);
         return ResponseEntity.ok(agenceService.searchBranchesByName(nomAgence));
-    }
-
-    /**
-     * GET /api/v1/branches/zone/{zone} : Retrieves branches by their zone.
-     *
-     * @param zone The zone of the branches to retrieve.
-     * @return A list of branches in the specified zone.
-     */
-    @GetMapping("/zone/{zone}")
-    public ResponseEntity<List<BranchDTO>> getBranchesByZone(
-            @PathVariable String zone) {
-        log.info("REST request to get Branches with zone: {}", zone);
-        return ResponseEntity.ok(agenceService.getBranchesByZone(zone));
-    }
-
-    /**
-     * GET /api/v1/branches/groupe/{groupe} : Retrieves branches by their group.
-     *
-     * @param groupe The group of the branches to retrieve.
-     * @return A list of branches in the specified group.
-     */
-    @GetMapping("/groupe/{groupe}")
-    public ResponseEntity<List<BranchDTO>> getBranchesByGroupe(
-            @PathVariable String groupe) {
-        log.info("REST request to get Branches with groupe: {}", groupe);
-        return ResponseEntity.ok(agenceService.getBranchesByGroupe(groupe));
-    }
-
-    /**
-     * GET /api/v1/branches/localite/{localite} : Retrieves branches by their locality.
-     *
-     * @param localite The locality of the branches to retrieve.
-     * @return A list of branches in the specified locality.
-     */
-    @GetMapping("/localite/{localite}")
-    public ResponseEntity<List<BranchDTO>> getBranchesByLocalite(
-            @PathVariable String localite) {
-        log.info("REST request to get Branches with localite: {}", localite);
-        return ResponseEntity.ok(agenceService.getBranchesByLocalite(localite));
     }
 
     /**
@@ -153,21 +119,6 @@ public class BrancheController {
     }
 
     /**
-     * GET /api/v1/branches/statut/{statut}/groupe/{groupe} : Retrieves branches by status and group.
-     *
-     * @param statut The status of the branches to retrieve.
-     * @param groupe The group of the branches to retrieve.
-     * @return A list of branches with the specified status and group.
-     */
-    @GetMapping("/statut/{statut}/groupe/{groupe}")
-    public ResponseEntity<List<BranchDTO>> getBranchesBystatutAndGroupe(
-            @PathVariable String statut,
-            @PathVariable String groupe) {
-        log.info("REST request to get Branches with statut: {} and groupe: {}", statut, groupe);
-        return ResponseEntity.ok(agenceService.getBranchesBystatutAndGroupe(statut, groupe));
-    }
-
-    /**
      * GET /api/v1/branches/simpleInfo : Retrieves simplified branch information with pagination and sorting.
      *
      * @param page The page number to retrieve (default is 0).
@@ -184,4 +135,12 @@ public class BrancheController {
         Pageable pageable = PageRequest.of(page, size, Sort.by(sort));
         return ResponseEntity.ok(agenceService.getAllBranchesSimple(pageable));
     }
+
+    @GetMapping("/simpleInfo/{codeAgence}")
+    public ResponseEntity<BranchSimpleDTO> getSimpleInfoBranchById(
+            @PathVariable String codeAgence) {
+        log.info("REST request to get Simple Info Branch with code: {}", codeAgence);
+        return ResponseEntity.ok(agenceService.getSimpleInfoBranchById(codeAgence));
+    }
+
 }
